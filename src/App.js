54214e375935169692f0bd282e8178425c48df07
Heon Soo Park <hspark220@gmail.com>
2023-03-29 08:07:01 -0400

    Add summary box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
 import './styles/App.css';
+import Header from './components/Header';
+import Main from './components/Main';
 
 class App extends Component {
   constructor (props) {
@@ -30,6 +31,7 @@ class App extends Component {
       <div className="App">
         <h1 className='title'>cv-application</h1>
         <Header />
+        <Main />
       </div>
     )
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/Main.css';
 
 class Main extends Component {
     constructor(props) {
@@ -9,8 +10,10 @@ class Main extends Component {
 
         return (
             <div className='main'>
-
+                <textarea type="text" placeholder='summary' className='summary'/>
             </div>
         )
     }
 }
+
+export default Main;
